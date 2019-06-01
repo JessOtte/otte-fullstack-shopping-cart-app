d305399ea2dc356418c7adfc066936a487b6e795
Jess <jessicaotte@ymail.com>
2019-06-01 16:37:30 -0400

    first commit

---
+++ Public/components/cart-list/cartList.js
@@ -0,0 +1,389 @@
+function ItemList (CartService) {
+  const ctrl = this;
+  ctrl.cart= [];
+
+  ctrl.cartList = () => {
+    CartService.getCart()
+
+    .then( (data) => {
+      console.log(data);
+
+      data.forEach(function(item) {
+        let cartObj = {
+          id: item.id,
+          product: item.product,
+          price: item.price,
+          count: item.count,
+          img: item.img
+        }
+        ctrl.cart.push(cartObj);
+      })
+
+    })
+
+    .catch( (error) => {
+      console.log(error);
+  })
+
+  }  
+// Constructor
+ctrl.saveCart = (product, price, count, img) => {
+  let item  = {
+      product: product,
+      price: price,
+      count: count,
+      image: img
+  }
+    // Save cart
+  CartService.sessionStorage.pushToCart('ctrl.cart', JSON.stringify(item))
+  CartService.sessionStorage.reloadData();
+}
+
+// function saveCart() {
+//   sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+// }
+
+  // Load cart
+function loadCart() {
+  cart = JSON.parse(sessionStorage.getItem('ctrl.cart'));
+}
+if (sessionStorage.getItem("ctrl.cart") != null) {
+  loadCart();
+}
+
+
+// =============================
+// Public methods and propeties
+// =============================
+let obj = {};
+
+// Add to cart
+obj.addItemToCart = function(product, price, count) {
+  for(let item in cart) {
+    if(cart[item].product === product) {
+      cart[item].count ++;
+      saveCart();
+      return;
+    }
+  }
+  let item = new Item(product, price, count);
+  cart.push(item);
+  saveCart();
+}
+// Set count from item
+obj.setCountForItem = function(product, count) {
+  for(let i in cart) {
+    if (cart[i].product === product) {
+      cart[i].count = count;
+      break;
+    }
+  }
+};
+// Remove item from cart
+obj.removeItemFromCart = function(product) {
+    for(let item in cart) {
+      if(cart[item].product === product) {
+        cart[item].count --;
+        if(cart[item].count === 0) {
+          cart.splice(item, 1);
+        }
+        break;
+      }
+  }
+  saveCart();
+
+  
+    // ************************************************
+// Shopping Cart API
+// ************************************************
+
+  // Remove all items from cart
+  obj.removeItemFromCartAll = function(product) {
+    for(let item in cart) {
+      if(cart[item].product === product) {
+        cart.splice(item, 1);
+        break;
+      }
+    }
+    saveCart();
+  }
+
+  // Clear cart
+  obj.clearCart = function() {
+    cart = [];
+    saveCart();
+  }
+
+
+
+
+  // UPDATING THE DISPLAY TEXT ONLY
+  
+  // List cart
+  obj.listCart = function() {
+    let cartCopy = [];
+    for(i in cart) {
+      item = cart[i];
+      itemCopy = {};
+      for(p in item) {
+        itemCopy[p] = item[p];
+
+      }
+      itemCopy.total = Number(item.price * item.count).toFixed(2);
+      cartCopy.push(itemCopy)
+    }
+    return cartCopy;
+  }  
+  
+  // Count cart 
+  obj.totalCount = function() {
+    let totalCount = 0;
+    for(let item in cart) {
+      totalCount += cart[item].count;
+    }
+    return totalCount;
+  }
+
+  // Total cart
+  obj.totalCart = function() {
+    let totalCart = 0;
+    for(let item in cart) {
+      totalCart += cart[item].price * cart[item].count;
+    }
+    return Number(totalCart.toFixed(2));
+  }
+
+
+
+  // cart : Array
+  // Item : Object/Class
+  // addItemToCart : Function
+  // removeItemFromCart : Function
+  // removeItemFromCartAll : Function
+  // clearCart : Function
+  // countCart : Function
+  // totalCart : Function
+  // listCart : Function
+  // saveCart : Function
+  // loadCart : Function
+  return obj;
+};
+
+
+// *****************************************
+// Triggers / Events
+// ***************************************** 
+// Add item
+$('.add-to-cart').click(function(event) {
+  event.preventDefault();
+  let product = $(this).data('product');
+  let price = Number($(this).data('price'));
+  ctrl.cart.addItemToCart(product, price, 1);
+  displayCart();
+});
+
+// Clear items
+$('.clear-cart').click(function() {
+  ctrl.cart.clearCart();
+  displayCart();
+});
+
+
+function displayCart() {
+  let cartArray = ctrl.cart.listCart();
+  let output = "";
+  for(let i in cartArray) {
+    output += "<tr>"
+      + "<td>" + cartArray[i].product + "</td>" 
+      + "<td>(" + cartArray[i].price + ")</td>"
+      + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-product=" + cartArray[i].product + ">-</button>"
+      + "<input type='number' class='item-count form-control' data-product='" + cartArray[i].product + "' value='" + cartArray[i].count + "'>"
+      + "<button class='plus-item btn btn-primary input-group-addon' data-product=" + cartArray[i].product + ">+</button></div></td>"
+      + "<td><button class='delete-item btn btn-danger' data-product=" + cartArray[i].product + ">X</button></td>"
+      + " = " 
+      + "<td>" + cartArray[i].total + "</td>" 
+      +  "</tr>";
+  }
+  $('.show-cart').html(output);
+  $('.total-cart').html(ctrl.cart.totalCart());
+  $('.total-count').html(ctrl.cart.totalCount());
+}
+
+// Delete item button
+
+$('.show-cart').on("click", ".delete-item", function(event) {
+  let product = $(this).data('product')
+  ctrl.cart.removeItemFromCartAll(product);
+  displayCart();
+})
+
+
+// -1
+$('.show-cart').on("click", ".minus-item", function(event) {
+  let product = $(this).data('product')
+  ctrl.cart.removeItemFromCart(product);
+  displayCart();
+})
+// +1
+$('.show-cart').on("click", ".plus-item", function(event) {
+  let product = $(this).data('product')
+  ctrl.cart.addItemToCart(product);
+  displayCart();
+})
+
+// Item count input
+$('.show-cart').on("change", ".item-count", function(event) {
+   let product = $(this).data('product');
+   let count = Number($(this).val());
+   ctrl.cart.setCountForItem(product, count);
+  displayCart();
+});
+
+displayCart();
+
+
+
+
+ctrl.cartList();
+
+ 
+  }
+
+angular.module("CartApp")
+.component('cartList', {
+    controller: ItemList,
+    template: `
+
+
+
+
+<!--
+    <div class="container">
+    <div class="row text-center">
+
+    <div class="col" ng-repeat = "cartData in $ctrl.cartData">
+    <div class="card" style="width: 20rem;">
+  <div class="card-block">
+    <h5 class="card-title">{{cartData.product}}</h5>
+    <p class="card-text"><small class="text-muted">Price: $\{{cartData.price}}</small></p>
+    <br>
+    <p>Quantity: {{cartData.quantity}}</p>
+    <button>Edit quantity</button>
+    <br>
+    <a href="#" data-product="{{cartData.product}}" data-price="{{cartData.price}}" class="add-to-cart btn btn-primary" ng-click="">Add to Cart</a>
+  </div>
+    
+
+  </div>
+  </div>
+  </div>
+  </div> -->
+
+
+  <div class="container">
+  <div class="row text-center">
+
+  
+  <div class="col">
+  <div class="card" style="width: 20rem;">
+<img class="card-img-top" src="imgs/catfood.jpg" alt="Cat Food">
+<div class="card-block">
+
+<h4 id="product" class="card-title">Cat Food</h4>
+<p class="card-text">Price: $5.00</p>
+<button data-product="Cat-Food" data-price="5.00" class="add-to-cart btn btn-primary" ng-click="$ctrl.saveCart($ctrl.product, $ctrl.price, count, $ctrl.img)">Add to cart</a>
+</div>
+</div>
+</div>
+
+
+<div class="col">
+  <div class="card" style="width: 20rem;">
+<img class="card-img-top" src="imgs/dogfood.jpg" alt="Dog Food">
+<div class="card-block">
+<h4 class="card-title">Dog Food</h4>
+<p class="card-text">Price: $5.00</p>
+<a href="#" data-product="Dog-Food" data-price="5.00" class="add-to-cart btn btn-primary">Add to cart</a>
+</div>
+</div>
+</div>
+<div class="col">
+<div class="card" style="width: 20rem;">
+<img class="card-img-top" src="imgs/birdfood.jpg" alt="Bird Food">
+<div class="card-block">
+<h4 class="card-title">Bird Food</h4>
+<p class="card-text">Price: $10.00</p>
+<a href="#" data-product="Bird-Food" data-price="10.00" class="add-to-cart btn btn-primary">Add to cart</a>
+</div>
+</div>
+</div>
+<div class="col">
+<div class="card" style="width: 20rem;">
+<img class="card-img-top" src="imgs/fishfood.jpg" alt="Card image cap">
+<div class="card-block">
+<h4 class="card-title">Fish Food</h4>
+<p class="card-text">Price: $1.00</p>
+<a href="#" data-product="Fish-Food" data-price="1.00" class="add-to-cart btn btn-primary">Add to cart</a>
+</div>
+</div>
+</div>
+<div class="col">
+<div class="card" style="width: 20rem;">
+<img class="card-img-top" src="imgs/turtlefood.jpg" alt="Card image cap">
+<div class="card-block">
+<h4 class="card-title">Turtle Food</h4>
+<p class="card-text">Price: $4.00</p>
+<a href="#" data-product="Turtle-Food" data-price="4.00" class="add-to-cart btn btn-primary">Add to cart</a>
+</div>
+</div>
+</div> 
+
+<div class="col">
+<div class="card" style="width: 20rem;">
+<img class="card-img-top" src="imgs/rabbitfood.jpg" alt="Card image cap">
+<div class="card-block">
+<h4 class="card-title">Rabbit Food</h4>
+<p class="card-text">Price: $2.00</p>
+<a href="#" data-product="rabbit-Food" data-price="2.00" class="add-to-cart btn btn-primary">Add to cart</a>
+</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+<!-- Modal -->
+    <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+       <div class="modal-dialog" role="document">
+         <div class="modal-content">
+           <div class="modal-header">
+             <h5 class="modal-title" id="exampleModalLabel">Cart</h5>
+             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+             </button>
+           </div>
+           <div class="modal-body">
+             <table class="show-cart table">                
+             </table>
+             <div class="modal-total">Total price: $<span class="total-cart"></span></div>
+           </div>
+           <div class="modal-footer">
+             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+             <!-- <button type="button" class="btn btn-primary">Order now</button> -->
+             <button class="clear-cart btn btn-danger">Clear Cart</button>
+           </div>
+         </div>
+       </div>
+     </div>  
+
+
+    
+
+    
+    `
+})
---
+++ cartItems.js
@@ -0,0 +1,54 @@
+//import express
+const express = require("express");
+//add router for cart - splits routes into separate modules
+const cart = express.Router();
+
+
+const pg = require("pg");
+const pool = new pg.Pool({
+  user: "postgres",
+  password: "adrenalinerush8",
+  host: "localhost",
+  port: 5432,
+  database: "ExpressShopDB",
+  ssl: false
+});
+
+cart.get("/", (req, res) => {
+    pool.query("SELECT * FROM shoppingcart;")
+    .then ( (results) => {
+        res.send(results.rows);
+    })
+    .catch ( (err) => {
+        res.send(err);
+    })
+});
+
+cart.post("/", (req, res) => {
+    let data = req.body;
+    pool.query("INSERT INTO shoppingcart (product, price, count, img) values($1::text, $2::float, $3::int, $4::text)",[data.product, data.price, data.count, data.img])
+    .then( () => {
+        res.status(201);
+        res.send(data.body);
+    })
+});
+
+cart.put("/:id", (req, res) => {
+    pool.query("UPDATE shoppingcart SET count=$1::int WHERE id=$2::int", [req.body.count, req.body.id])
+    .then( () => {
+        res.send("Cart Item Updated!");
+    })
+    .catch( (err) => {
+        res.send(err);
+    })
+});
+
+cart.delete("/:id", (req, res) => {
+    pool.query("DELETE FROM shoppingcart WHERE id=$1::int", [req.params.id])
+    .then( () => {
+        res.status(201);
+        res.send("Cart Item Deleted!");
+    })
+});
+
+module.exports = cart;
